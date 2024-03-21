986bbb9373a4c7a191e7be12d87cca1e8d884c94
Ta Cuong <cuongvt@appota.com>
2024-03-21 15:48:38 +0700

    Update avatar image

--- reactjs/src/pages/profile/index.tsx
+++ reactjs/src/pages/profile/index.tsx
@@ -1,13 +1,38 @@
 import axios from "axios";
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useKeycloak } from "@react-keycloak/web";
+import { Ref, useRef, useState, useEffect } from "react";
 
 import "./index.css";
 
 const Profile = () => {
   const navigate = useNavigate();
   const { keycloak } = useKeycloak();
+  const fileRef: Ref<any> = useRef(null);
+  const [avatarImage, setAvatarImage] = useState(keycloak.tokenParsed?.avatar);
+
+  const handleChangeFile = async (e: any) => {
+    const formData = new FormData();
+    formData.append("files", e.target.files[0]);
+
+    const { data } = await axios.post(
+      `${process.env.REACT_APP_API_BASE_URL}/file/upload`,
+      formData,
+      { headers: { "content-type": "multipart/form-data" } }
+    );
+    setAvatarImage(data.data.urls[0]);
+
+    const res = await axios.post(
+      `${process.env.REACT_APP_KEYCLOAK_URL}/realms/${process.env.REACT_APP_KEYCLOAK_REALM}/account`,
+      {
+        email: keycloak.tokenParsed?.email,
+        attributes: { avatar: data.data.urls },
+        firstName: keycloak.tokenParsed?.given_name,
+        lastName: keycloak.tokenParsed?.family_name,
+      }
+    );
+    console.log(res);
+  };
 
   useEffect(() => {
     axios
@@ -22,15 +47,24 @@ const Profile = () => {
     <div className="profile__container">
       <div className="profile__card">
         <div className="card__avatar">
+          <input
+            type="file"
+            ref={fileRef}
+            onChange={handleChangeFile}
+            style={{ display: "none" }}
+          />
           <img
             alt="avatar"
-            onClick={() => keycloak.accountManagement()}
-            src={
-              keycloak.tokenParsed?.avatar || "images/a-boy-simple-avatar.webp"
-            }
+            onClick={() => fileRef.current.click()}
+            src={avatarImage || "images/a-boy-simple-avatar.webp"}
           />
         </div>
-        <div className="card__title">{keycloak.tokenParsed?.name}</div>
+        <div
+          className="card__title"
+          onClick={() => keycloak.accountManagement()}
+        >
+          {keycloak.tokenParsed?.name}
+        </div>
         <div className="card__subtitle">{keycloak.tokenParsed?.email}</div>
         <div className="card__button-group">
           <button className="card__btn" onClick={() => navigate("/")}>
